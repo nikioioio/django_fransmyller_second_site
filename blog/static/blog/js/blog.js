6a7caf1c863364cc60ad6ea87334a93a319a534a
nikio <ked_nik@mail.ru>
2021-06-20 18:31:41 +0600

    Доделываю детельный пост.

--- blog/static/blog/js/blog.js
+++ blog/static/blog/js/blog.js
@@ -3,13 +3,22 @@ $(document).ready(function () {
     var el_def = $(".btn:first");
     var defHeiht = el_def.height()
 
-    $('.btn').click(function (event) {
+    // $('.btn').click(function (event) {
+    //
+    //     if ($(event.delegateTarget).height()>defHeiht){
+    //          $(event.delegateTarget).height(defHeiht)
+    //     }else{
+    //         $(event.delegateTarget).height(defHeiht).animate({ height: 100 })
+    //     }
+    // })
 
-        if ($(event.delegateTarget).height()>defHeiht){
+
+    $('.btn').hover(function (event) {
+            $(event.delegateTarget).height(defHeiht).animate({ height: 150},1 )
+            $(event.delegateTarget).children(".hidden_elements").css({'display':'flex'})
+    },function (event){
              $(event.delegateTarget).height(defHeiht)
-        }else{
-            $(event.delegateTarget).height(defHeiht).animate({ height: 100 })
-        }
+            $(event.delegateTarget).children(".hidden_elements").css({'display':'none'})
     })
 })
 
--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def starting_page(request):
-    return render(request, "blog/index.html")
+    return render(request, "base.html")
 
 def posts(request):
-    pass
+    return render(request, "blog/index.html")
 
-def post_detail(request, ):
-    pass
+def post_detail(request, slug):
+    return render(request,"blog/post-detail.html")
